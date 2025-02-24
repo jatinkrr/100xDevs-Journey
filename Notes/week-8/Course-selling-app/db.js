6f8a7ca1d2eea7445246e6a873366a43d151cb31
Jatin Kumar <jatinkr0104@gmail.com>
2025-02-24 21:16:39 +0530

    week-8

---
+++ Notes/week-8/Course-selling-app/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const user = new Schema({
+    name:String,
+    email:{type:String, unique:true},
+    password:String
+})
+
+const usermodel = mongoose.model('User-Data',user) 
+
+module.exports = {
+    usermodel
+}
