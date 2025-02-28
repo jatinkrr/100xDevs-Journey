f284e68408a2a6e1dcfe110433b7539629d34646
Jatin Kumar <jatinkr0104@gmail.com>
2025-02-28 20:06:02 +0530

    added admin Schema

--- Notes/week-8/cousesera_practise/routes/admin.js
+++ Notes/week-8/cousesera_practise/routes/admin.js
@@ -1,17 +1,61 @@
+
+
 const {Router} = require("express")
-
 const {adminmodel} = require("../db")
-
+const jwt = require("jsonwebtoken")
+const admin_secret = process.env.admin_secret
 const adminRoute = Router()
+const bcrypt = require("bcrypt")
 
-adminRoute.post("/signup",(req,res)=>{
-    
+adminRoute.post("/signup",async (req,res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const hashedpassword = await bcrypt.hash(password,5)
+try{
+    await adminmodel.create({
+        firstName,
+        lastName,
+        email,
+        password:hashedpassword
+    })
+    res.json("you are signed up")
+}catch(e){
+    res.json({
+        msg:"email already exits"
+    })
+}
 })
-adminRoute.post("/signin",(req,res)=>{
+adminRoute.post("/signin",async (req,res)=>{
+    const email = req.body.email
+    const password = req.body.password
 
+    const user=await adminmodel.findOne({
+        email
+    })
+    if(!user){
+        res.json({
+            msg:"email not found"
+        })
+    }
+    const matchPassword =await bcrypt.compare(password,user.password)
+    if(matchPassword){
+        const token = jwt.sign({
+            id:user._id
+        },admin_secret)
+        res.json({
+            token:token
+        })
+    }else{
+        res.json({
+            msg:"invalid credantials"
+        })
+    }
 })
 adminRoute.post("/course",(req,res)=>{
-
+    
 })
 adminRoute.put("/purchase",(req,res)=>{
 
