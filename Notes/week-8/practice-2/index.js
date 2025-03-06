0012de14d4bd9d1a7b23133f1421911a3d5917d1
Jatin Kumar <jatinkr0104@gmail.com>
2025-03-06 16:33:08 +0530

    added admin schema

--- Notes/week-8/practice-2/index.js
+++ Notes/week-8/practice-2/index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const {userRoute} = require("./routes/user")
+const {adminRoute} = require("./routes/admin")
 const app = express()
 
 
@@ -10,6 +11,7 @@ app.use(express.json())
 
 
 app.use("/user",userRoute)
+app.use("/admin",adminRoute)
 
 
 
---
+++ Notes/week-8/practice-2/routes/admin.js
@@ -0,0 +1,66 @@
+const {Router} = require("express")
+const {adminmodel} = require("../db")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const admin_JWT_SECRET = process.env.admin_JWT_SECRET
+const adminRoute = Router()
+
+adminRoute.post("/signup",async (req,res)=>{
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email 
+    const password = req.body.password;
+
+try{
+    const hashedpassword = await bcrypt.hash(password,5)
+   await adminmodel.create({
+        firstName,
+        lastName,
+        email,
+        password:hashedpassword
+    })
+    res.json({
+        msg:"you are signed up"
+    })
+
+}catch(e){
+    res.json({
+        msg:"email already exists"
+    })
+}
+})
+
+adminRoute.post("/signin",async (req,res)=>{
+
+    const email = req.body.email 
+    const password = req.body.password
+
+    
+    const admin=await adminmodel.findOne({
+        email
+    })
+    if(!admin){
+        res.json({
+            msg:"admin not exists in DataBase"
+        })
+    }
+    const matchPassword = await bcrypt.compare(password, admin.password)
+
+    if(matchPassword){
+        const token = jwt.sign({
+            id:admin._id
+        },admin_JWT_SECRET)
+        res.json({
+           token : token
+        })
+    }else{
+        res.json({
+            msg:"invalid credantials"
+        })
+    }
+
+})
+
+module.exports = {
+    adminRoute
+}
