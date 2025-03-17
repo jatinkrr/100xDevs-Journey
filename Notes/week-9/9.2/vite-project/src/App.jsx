25883db5f4345a57e2cfd7e93ea8daab4dc1e594
Jatin Kumar <jatinkr0104@gmail.com>
2025-03-17 22:15:19 +0530

    error in clock

--- Notes/week-9/9.2/vite-project/src/App.jsx
+++ Notes/week-9/9.2/vite-project/src/App.jsx
@@ -3,38 +3,88 @@ import { useState } from 'react'
 
 function App() {
   return(
-    <Counter></Counter>  
+    <Clock></Clock>
+    // <Counter></Counter>  
   )
 
 }
 
 
-function Counter(){
+function Clock(){
 
-  let [count,setCount]= useState(0)
+  let [hours,sethours] = useState(0)
+  let[minute,setminute] = useState(0)
+  let[second,setsecond] = useState(0)
 
-  function increaseCount(){
-    setCount(count+1)
-  }
+  
+   setInterval(()=>{
+      sethours(hours+1)
+    },3600000)
+
+    setInterval(() => {
+      setminute(minute+1)
+    }, 60000);
+    
+    setInterval(() => {
+      setsecond(second+1)
+    }, 1000);
+    
 
 
-  function decreaseCount(){
-    setCount(count - 1 )
-  }
-
-  function resetCount(){
-    setCount( 0 )
-  }
 
   return (
-    <div>
-        <h1>{count}</h1>
-        <button onClick={increaseCount}>Increase count</button>
-        <button onClick={decreaseCount}>Decrease count</button>
-        <button onClick={resetCount}>Reset count</button>
-    </div>
+   <div>
+     <span> <h1>{hours}</h1></span>
+     <span> <h1>{minute}</h1></span>
+     <span> <h1>{second}</h1></span>
+   </div>
+    
+      // {/* <button onClick={increaseHours}>startClock</button> */}
+    
   )
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Counter(){
+
+//   let [count,setCount]= useState(0)
+
+//   function increaseCount(){
+//     setCount(count+1)
+//   }
+
+
+//   function decreaseCount(){
+//     setCount(count - 1 )
+//   }
+
+//   function resetCount(){
+//     setCount( 0 )
+//   }
+
+//   return (
+//     <div>
+//         <h1>{count}</h1>
+//         <button onClick={increaseCount}>Increase count</button>
+//         <button onClick={decreaseCount}>Decrease count</button>
+//         <button onClick={resetCount}>Reset count</button>
+//     </div>
+//   )
+
+// }
 
 export default App
