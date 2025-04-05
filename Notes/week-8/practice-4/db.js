0645aac163ae72a23b68e88d69e7ff067690a275
Jatin Kumar <jatinkr0104@gmail.com>
2025-04-05 20:12:19 +0530

    practise-4 mongo db

---
+++ Notes/week-8/practice-4/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+const ObjectId = mongoose.ObjectId
+
+const user = new Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String,
+})
+
+const admin = new Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String,
+})
+
+const userModel = mongoose.model("userData",user)
+const adminModel= mongoose.model("adminData",admin)
+
+module.exports={
+    userModel,
+    adminModel
+}
---
+++ Notes/week-8/practice-4/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const express = require("express")
+
+
+const app = express()
+
+
+async function main(){
+    await mongoose.connect(process.env.mongooseUrl)
+    app.listen(3000)
+    console.log("connected to 3000 server")
+} 
+
+main() 
